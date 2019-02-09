2c42c12110a736c4de4ef98ba4115b78043aa821
ahmedash11 <ahmedashh11@gmail.com>
2019-02-09 10:22:33 +0200

    Updated Swagger.Json File

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const swaggerUi = require("swagger-ui-express");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-
-// import YAML from "yamljs";
-// import { errorHandler } from "./utilities/errorHandler";
-// import routesV1 from "./routes.v1";
-
-//Require environment files
-// require("dotenv").config();
-
-// // Require cron jobs
-// require("./cronJobs");
-
 
 //Swagger Setup
 const swaggerDocument = require("./swagger.json");
